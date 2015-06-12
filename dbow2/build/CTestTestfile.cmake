a68a72e171e6292056f07a4d9abf5acc19573c3a
Patrick Geneva <patfgeneva@gmail.com>
2015-06-12 16:41:39 -0400

    Moved - DBoW2 to lower case package

---
+++ dbow2/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/patrick/catkin_ws/src/orb_slam/DBoW2
+# Build directory: /home/patrick/catkin_ws/src/orb_slam/DBoW2/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
---
+++ dbow2/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/lib:/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/lib/pkgconfig:/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/lib/x86_64-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PYTHONPATH="/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/patrick/catkin_ws/src/orb_slam/DBoW2/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/patrick/catkin_ws/src/orb_slam/DBoW2:$ROS_PACKAGE_PATH"
